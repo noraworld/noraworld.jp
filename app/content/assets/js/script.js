ca38e58c4a34326a4e58eee3b9b34122b750c228
noraworld <noraworld@mail.com>
2016-10-23 13:37:58 +0100

    :sparkles: Append scroll and slider animation

--- app/content/assets/js/script.js
+++ app/content/assets/js/script.js
@@ -1,15 +1,55 @@
 (function() {
 
-  "use strict";
+  'use strict';
 
-  var scrollButton = document.querySelector('#profile');
-  var rect = scrollButton.getBoundingClientRect();
-  var profilePositionX = rect.left + window.pageXOffset;
-  var profilePositionY = rect.top  + window.pageYOffset;
+  smoothScroll.init();
+
+  var scrollAnimationFlag = {
+    profile:    null,
+    works:      null,
+    activities: null
+  };
+
+  if (document.body.scrollTop < 550) {
+    document.querySelector('#profile .contents-box .contents-wrapper').style.opacity = 0;
+    scrollAnimationFlag['profile'] = true;
+  }
+  if (document.body.scrollTop < 1156) {
+    document.querySelector('#works .contents-box .contents-wrapper').style.opacity = 0;
+    scrollAnimationFlag['works'] = true;
+  }
+  if (document.body.scrollTop < 1740) {
+    document.querySelector('#activities .contents-box .contents-wrapper').style.opacity = 0;
+    scrollAnimationFlag['activities'] = true;
+  }
 
   document.querySelector('.scroll-down').addEventListener('click', (event) => {
     event.preventDefault();
-    window.scrollTo(profilePositionX, profilePositionY);
+    smoothScroll.animateScroll(document.querySelector('#profile'));
   });
+
+  window.addEventListener('scroll', () => {
+
+    animateSlider(document.querySelector('#profile .contents-box .contents-wrapper'), 550, 'profile');
+    animateSlider(document.querySelector('#works .contents-box .contents-wrapper'), 1156, 'works');
+    animateSlider(document.querySelector('#activities .contents-box .contents-wrapper'), 1740, 'activities');
+
+  });
+
+  var animateSlider = (element, position, flag) => {
+    if (document.body.scrollTop > position && scrollAnimationFlag[flag]) {
+      element.animate({
+        opacity: [0, 1],
+        transform: ['translate(100%, 0)', 'translate(0, 0)'],
+      }, {
+        duration: 500,
+        iterations: 1,
+        easing: 'ease-in-out',
+      }).onfinish = () => {
+        element.style.opacity = 1;
+        scrollAnimationFlag[flag] = false;
+      }
+    }
+  }
 
 }());
