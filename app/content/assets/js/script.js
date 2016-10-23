5473e6c24a6783fea98a5b7661378d818280c5af
noraworld <noraworld@mail.com>
2016-10-23 13:53:44 +0100

    :pencil2: Show left side content from this side

--- app/content/assets/js/script.js
+++ app/content/assets/js/script.js
@@ -10,6 +10,7 @@
     activities: null
   };
 
+  // if a script is enabled, hide content to show with animations
   if (document.body.scrollTop < 550) {
     document.querySelector('#profile .contents-box .contents-wrapper').style.opacity = 0;
     scrollAnimationFlag['profile'] = true;
@@ -23,24 +24,25 @@
     scrollAnimationFlag['activities'] = true;
   }
 
+  // click down arrow to scroll down to profile content
   document.querySelector('.scroll-down').addEventListener('click', (event) => {
     event.preventDefault();
     smoothScroll.animateScroll(document.querySelector('#profile'));
   });
 
+  // work with each scroll action
   window.addEventListener('scroll', () => {
-
-    animateSlider(document.querySelector('#profile .contents-box .contents-wrapper'), 550, 'profile');
-    animateSlider(document.querySelector('#works .contents-box .contents-wrapper'), 1156, 'works');
-    animateSlider(document.querySelector('#activities .contents-box .contents-wrapper'), 1740, 'activities');
-
+    animateSlider(document.querySelector(   '#profile .contents-box .contents-wrapper'),  550,    'profile',  '100%');
+    animateSlider(document.querySelector(     '#works .contents-box .contents-wrapper'), 1156,      'works', '-100%');
+    animateSlider(document.querySelector('#activities .contents-box .contents-wrapper'), 1740, 'activities',  '100%');
   });
 
-  var animateSlider = (element, position, flag) => {
+  // show content with slider animations
+  var animateSlider = (element, position, flag, direction) => {
     if (document.body.scrollTop > position && scrollAnimationFlag[flag]) {
       element.animate({
         opacity: [0, 1],
-        transform: ['translate(100%, 0)', 'translate(0, 0)'],
+        transform: ['translate(' + direction + ', 0)', 'translate(0, 0)'],
       }, {
         duration: 500,
         iterations: 1,
