91ca8da0d93a7e54d798859a2c97cf20a1d2bc71
noraworld <noraworld@mail.com>
2016-11-18 06:57:07 +0000

    :sparkles: Imprement scroll position algorithm for slider animation

--- app/content/assets/js/script.js
+++ app/content/assets/js/script.js
@@ -9,17 +9,20 @@
     works:      null,
     activities: null
   };
+  var profilePosition    = document.querySelector(   '#profile .contents-box').getBoundingClientRect().top + window.pageYOffset + document.querySelector(   '#profile .contents-box').offsetHeight / 2;
+  var worksPosition      = document.querySelector(     '#works .contents-box').getBoundingClientRect().top + window.pageYOffset + document.querySelector(     '#works .contents-box').offsetHeight / 2;
+  var activitiesPosition = document.querySelector('#activities .contents-box').getBoundingClientRect().top + window.pageYOffset + document.querySelector('#activities .contents-box').offsetHeight / 2;
 
   // if a script is enabled, hide content to show with animations
-  if (document.body.scrollTop < 280) {
+  if (window.pageYOffset + window.innerHeight < profilePosition) {
     document.querySelector('#profile .contents-box .contents-wrapper').style.opacity = 0;
     scrollAnimationFlag['profile'] = true;
   }
-  if (document.body.scrollTop < 1156) {
+  if (window.pageYOffset + window.innerHeight < worksPosition) {
     document.querySelector('#works .contents-box .contents-wrapper').style.opacity = 0;
     scrollAnimationFlag['works'] = true;
   }
-  if (document.body.scrollTop < 1740) {
+  if (window.pageYOffset + window.innerHeight < activitiesPosition) {
     document.querySelector('#activities .contents-box .contents-wrapper').style.opacity = 0;
     scrollAnimationFlag['activities'] = true;
   }
@@ -32,14 +35,14 @@
 
   // work with each scroll action
   window.addEventListener('scroll', () => {
-    animateSlider(document.querySelector(   '#profile .contents-box .contents-wrapper'),  280,    'profile',  '100%');
-    animateSlider(document.querySelector(     '#works .contents-box .contents-wrapper'), 1156,      'works', '-100%');
-    animateSlider(document.querySelector('#activities .contents-box .contents-wrapper'), 1740, 'activities',  '100%');
+    animateSlider(document.querySelector(   '#profile .contents-box .contents-wrapper'),     profilePosition,    'profile',  '100%');
+    animateSlider(document.querySelector(     '#works .contents-box .contents-wrapper'),       worksPosition,      'works', '-100%');
+    animateSlider(document.querySelector('#activities .contents-box .contents-wrapper'),  activitiesPosition, 'activities',  '100%');
   });
 
   // show content with slider animations
   var animateSlider = (element, position, flag, direction) => {
-    if (document.body.scrollTop > position && scrollAnimationFlag[flag]) {
+    if (window.pageYOffset + window.innerHeight > position && scrollAnimationFlag[flag]) {
       element.animate({
         opacity: [0, 1],
         transform: ['translate(' + direction + ', 0)', 'translate(0, 0)'],
