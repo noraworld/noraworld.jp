ba7df1d1fe355fc68e8a2e35bcfa793a60affcdb
noraworld <noraworld@mail.com>
2016-11-20 15:30:01 +0000

    :bug: Fix canvas height

--- app/content/src/js/smartphone.js
+++ app/content/src/js/smartphone.js
@@ -4,7 +4,7 @@
 
   // fixs to rattle caused by view height (vh) on iOS
   if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/)) {
-    document.querySelector('#canvas').style.height = window.innerHeight;
+    document.querySelector('#canvas').style.height = window.innerHeight + 'px';
   }
 
 })();
