33ec92eba36c8896c70a1c5a24c341f2f1938f04
noraworld <noraworld@mail.com>
2016-11-20 17:58:20 +0000

    :pencil2: Change scroll down arrow color for each sketch

--- app/content/src/js/sketch.js
+++ app/content/src/js/sketch.js
@@ -1,3 +1,6 @@
+// This code is spaghetti code :-(
+// Please teach me the best practice...
+
 // switch sketch between mobiles and PCs
 var isMobile = false;
 if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/)) {
@@ -6,8 +9,8 @@ if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobil
 
 // should push sketch name if added new sketch file
 var sketch = [
-  'bubble',
-  'ring'
+  ['bubble', '105'],
+  [  'ring', '255']
 ];
 var rand = Math.floor(Math.random() * sketch.length);
 
@@ -15,40 +18,50 @@ var setup = () => {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('canvas');
   if (!isMobile) {
-    switch (sketch[rand]) {
+    switch (sketch[rand][0]) {
       case 'bubble' : initializeBubbles();  break;
       case 'ring'   : initializeRings();    break;
       default       : initializeRings();
     }
+    changeArrowColor(sketch[rand][1]);
   }
   else {
     initializeBubbles();
+    changeArrowColor(sketch[0][1]);
   }
 }
 
 var draw = () => {
   if (!isMobile) {
-    switch (sketch[rand]) {
+    switch (sketch[rand][0]) {
       case 'bubble' : sketchBubbles();  break;
       case 'ring'   : sketchRings();    break;
       default       : sketchRings();
     }
+    changeArrowColor(sketch[rand][1]);
   }
   else {
     sketchBubbles();
+    changeArrowColor(sketch[0][1]);
   }
 }
 
 var windowResized = () => {
   if (!isMobile) {
     resizeCanvas(windowWidth, windowHeight);
-    switch (sketch[rand]) {
+    switch (sketch[rand][0]) {
       case 'bubble' : resizeBubbles();  break;
       case 'ring'   : resizeRings();    break;
       default       : resizeRings();
     }
+    changeArrowColor(sketch[rand][1]);
   }
   else {
     resizeBubbles();
+    changeArrowColor(sketch[0][1]);
   }
+}
+
+var changeArrowColor = (color) => {
+  document.querySelector('.scroll-down').style.color = 'rgba(' + color + ', ' + color + ', ' + color + ', .8)';
 }
