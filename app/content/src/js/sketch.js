d30a92cda24a4c153828dda89dda561a18bb77bb
noraworld <noraworld@mail.com>
2016-11-20 17:21:40 +0000

    :pencil2: Random switch sketchs for PC view

--- app/content/src/js/sketch.js
+++ app/content/src/js/sketch.js
@@ -1,13 +1,25 @@
-var isSmartphone = false;
+// switch sketch between mobiles and PCs
+var isMobile = false;
 if (navigator.userAgent.match(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/)) {
-  isSmartphone = true;
+  isMobile = true;
 }
+
+// should push sketch name if added new sketch file
+var sketch = [
+  'bubble',
+  'ring'
+];
+var rand = Math.floor(Math.random() * sketch.length);
 
 var setup = () => {
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('canvas');
-  if (!isSmartphone) {
-    initializeRings();
+  if (!isMobile) {
+    switch (sketch[rand]) {
+      case 'bubble' : initializeBubbles();  break;
+      case 'ring'   : initializeRings();    break;
+      default       : initializeRings();
+    }
   }
   else {
     initializeBubbles();
@@ -15,8 +27,12 @@ var setup = () => {
 }
 
 var draw = () => {
-  if (!isSmartphone) {
-    sketchRings();
+  if (!isMobile) {
+    switch (sketch[rand]) {
+      case 'bubble' : sketchBubbles();  break;
+      case 'ring'   : sketchRings();    break;
+      default       : sketchRings();
+    }
   }
   else {
     sketchBubbles();
@@ -24,9 +40,13 @@ var draw = () => {
 }
 
 var windowResized = () => {
-  if (!isSmartphone) {
+  if (!isMobile) {
     resizeCanvas(windowWidth, windowHeight);
-    resizeRings();
+    switch (sketch[rand]) {
+      case 'bubble' : resizeBubbles();  break;
+      case 'ring'   : resizeRings();    break;
+      default       : resizeRings();
+    }
   }
   else {
     resizeBubbles();
