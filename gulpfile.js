86b7bb182bc17c14ffc96c2914755b80c17d4093
noraworld <noraworld@mail.com>
2016-11-20 13:18:43 +0000

    :twisted_rightwards_arrows: Change CSS and JS files from assets/ to src/

--- gulpfile.js
+++ gulpfile.js
@@ -7,27 +7,27 @@ var autoprefixer = require('gulp-autoprefixer');
 var minifycss    = require('gulp-minify-css');
 
 gulp.task('css', function() {
-  gulp.src('./app/content/assets/css/*.css')
+  gulp.src('./app/content/src/css/*.css')
       .pipe(autoprefixer())
       .pipe(concat('all.css'))
       .pipe(minifycss())
-      .pipe(gulp.dest('./app/content/dist'));
+      .pipe(gulp.dest('./app/content/assets/css'));
 });
 
 gulp.task('js', function() {
-  gulp.src('./app/content/assets/js/*')
+  gulp.src('./app/content/src/js/*')
       .pipe(plumber())
       .pipe(babel({
         presets: ['es2015']
       }))
       .pipe(concat('all.js'))
       .pipe(uglify())
-      .pipe(gulp.dest('./app/content/dist'));
+      .pipe(gulp.dest('./app/content/assets/js'));
 });
 
 // TODO: cannot watch the CSS files, fix this
 gulp.task('watch', function() {
-  gulp.watch('./app/content/assets/js/*', ['css', 'js']);
+  gulp.watch(['./app/content/src/css/*.css', './app/content/src/js/*.js'], ['css', 'js']);
 });
 
 gulp.task('default', ['css', 'js', 'watch']);
