6bc6e1c97ff894f46da54be2d08950f8c3d1be74
noraworld <noraworld@mail.com>
2016-11-20 12:48:11 +0000

    :sparkles: Add CSS gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,33 @@
-var gulp    = require('gulp');
-var concat  = require('gulp-concat');
-var babel   = require('gulp-babel');
-var uglify  = require('gulp-uglify');
-var plumber = require('gulp-plumber');
+var gulp         = require('gulp');
+var concat       = require('gulp-concat');
+var babel        = require('gulp-babel');
+var uglify       = require('gulp-uglify');
+var plumber      = require('gulp-plumber');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss    = require('gulp-minify-css');
+
+gulp.task('css', function() {
+  gulp.src('./app/content/assets/css/*.css')
+      .pipe(autoprefixer())
+      .pipe(concat('all.css'))
+      .pipe(minifycss())
+      .pipe(gulp.dest('./app/content/dist'));
+});
 
 gulp.task('js', function() {
   gulp.src('./app/content/assets/js/*')
       .pipe(plumber())
-      .pipe(babel({presets: ['es2015']}))
+      .pipe(babel({
+        presets: ['es2015']
+      }))
       .pipe(concat('all.js'))
       .pipe(uglify())
       .pipe(gulp.dest('./app/content/dist'));
 });
 
+// TODO: cannot watch the CSS files, fix this
 gulp.task('watch', function() {
-  gulp.watch('./app/content/assets/js/*', ['js']);
+  gulp.watch('./app/content/assets/js/*', ['css', 'js']);
 });
 
-gulp.task('default', ['js', 'watch']);
+gulp.task('default', ['css', 'js', 'watch']);
